2f776cf9c4e62f9dc271d9aca00fc68cdca8393a
Acround <acround@gmail.com>
2023-11-14 18:56:08 +0100

    Skeleton

---
+++ web/protected/config/database.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	'connectionString' => 'mysql:host=localhost;dbname=optima',
+	'emulatePrepare' => true,
+	'username' => 'acround',
+	'password' => '12cool09',
+	'charset' => 'utf8',
+);
